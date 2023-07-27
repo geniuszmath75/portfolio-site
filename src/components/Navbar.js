fc3759e7d172083cb851b9d249fe9796981f5cb4
Damian <88547250+geniuszmath75@users.noreply.github.com>
2023-07-27 15:36:58 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,8 @@ import { Link } from "react-router-dom";
 import { MdHome, MdAssignment, MdPerson } from "react-icons/md";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
-import { useState } from "react";
 
 function Navbar() {
-  const [hover, setHover] = useState(false);
-  const handleMouseOver = () => setHover(false);
-  const handleMouseOut = () => setHover(true);
-
   return (
     <IconContext.Provider value={{ className: "menu-item-img" }}>
       <>
