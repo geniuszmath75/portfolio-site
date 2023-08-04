da71bc88d6716030eab4dde68d2860fb3cf6021d
geniuszmath75 <geniuszmath75@gmail.com>
2023-08-04 16:52:31 +0200

    Updating Navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { MdHome, MdAssignment, MdPerson } from "react-icons/md";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
+import { useEffect } from "react";
 
 function Navbar() {
+  const { hash } = useLocation();
+  useEffect(() => {
+    if (hash) {
+      const targetElement = document.getElementById(hash.substring(1));
+      targetElement?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [hash]);
+
   return (
     <IconContext.Provider value={{ className: "menu-item-img" }}>
       <>
@@ -13,17 +22,17 @@ function Navbar() {
               <li className="menu-item">
                 <Link to="/" className="nav-links">
                   <MdHome />
-                  <div className="menu-item-text">Home</div>
+                  <div className="menu-item-text">About Me</div>
                 </Link>
               </li>
               <li className="menu-item">
-                <Link to="/" className="nav-links">
+                <Link to="#projects" className="nav-links">
                   <MdAssignment />
                   <div className="menu-item-text">Projects</div>
                 </Link>
               </li>
               <li className="menu-item">
-                <Link to="/" className="nav-links">
+                <Link to="#contact" className="nav-links">
                   <MdPerson />
                   <div className="menu-item-text">Contact</div>
                 </Link>
