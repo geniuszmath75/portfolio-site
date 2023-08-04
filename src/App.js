a5798c5c114c5eafb0f384c2df28a5a1d8bed04e
geniuszmath75 <geniuszmath75@gmail.com>
2023-08-04 16:57:02 +0200

    Updating app component.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Projects from './components/Projects';
+import AboutMe from './components/home/AboutMe/AboutMe.js';
+import Projects from './components/home/Projects/Projects.js';
+import Contact from './components/home/Contact/Contact';
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Home />
+      <AboutMe />
       <Projects />
+      <Contact />
     </Router>
   );
 }
