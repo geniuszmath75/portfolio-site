19b03692976a156e544835bfc155645091f5ca87
geniuszmath75 <geniuszmath75@gmail.com>
2023-08-07 16:16:38 +0200

    Updating components props.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import AboutMe from './components/home/AboutMe/AboutMe.js';
-import Projects from './components/home/Projects/Projects.js';
-import Contact from './components/home/Contact/Contact';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./components/home/Navbar/Navbar";
+import AboutMe from "./components/home/AboutMe/AboutMe.js";
+import Projects from "./components/home/Projects/Projects.js";
+import Contact from "./components/home/Contact/Contact";
+import { navbarData } from "./components/home/Navbar/Data";
+import { aboutMeData } from "./components/home/AboutMe/Data";
+import { projectsData } from "./components/home/Projects/Data";
+import { contactData } from "./components/home/Contact/Data";
 
 function App() {
+  const [click, setClick] = useState(true);
+  const [polishVer, setPolishVer] = useState(true);
+
+  const handleClickPL = () => {
+    if (click) {
+      setClick(click);
+    } else {
+      setClick(!click);
+      setPolishVer(!click);
+    }
+  };
+
+  const handleClickEN = () => {
+    setClick(!click);
+    setPolishVer(!polishVer);
+  };
+
   return (
     <Router>
-      <Navbar />
-      <AboutMe />
-      <Projects />
-      <Contact />
+      <Navbar
+        handleClickEN={handleClickEN}
+        handleClickPL={handleClickPL}
+        click={click}
+        polishVer={polishVer}
+        {...navbarData}
+      />
+      <AboutMe polishVer={polishVer} {...aboutMeData} />
+      <Projects polishVer={polishVer} {...projectsData} />
+      <Contact polishVer={polishVer} {...contactData} />
     </Router>
   );
 }
