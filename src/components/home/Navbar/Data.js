9cab9bbfea3c7255d4d1ba2f3039861e7691922d
geniuszmath75 <geniuszmath75@gmail.com>
2023-08-07 16:12:36 +0200

    Adding polish and english version of navbar.

---
+++ src/components/home/Navbar/Data.js
@@ -0,0 +1,8 @@
+export const navbarData = {
+    homeBtnPl: "O Mnie",
+    projectsBtnPl: "Projekty",
+    contactBtnPl: "Kontakt",
+    homeBtnEn: "About Me",
+    projectsBtnEn: "Projects",
+    contactBtnEn: "Contact"
+}
--- src/components/home/Navbar/Navbar.js
+++ src/components/home/Navbar/Navbar.js
@@ -2,9 +2,21 @@ import { Link, useLocation } from "react-router-dom";
 import { MdHome, MdAssignment, MdPerson } from "react-icons/md";
 import "./Navbar.css";
 import { IconContext } from "react-icons";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function Navbar() {
+function Navbar({
+  homeBtnPl,
+  projectsBtnPl,
+  contactBtnPl,
+  homeBtnEn,
+  projectsBtnEn,
+  contactBtnEn,
+  click,
+  polishVer,
+  handleClickEN,
+  handleClickPL
+}) {
+  
   const { hash } = useLocation();
   useEffect(() => {
     if (hash) {
@@ -22,22 +34,38 @@ function Navbar() {
               <li className="menu-item">
                 <Link to="/" className="nav-links">
                   <MdHome />
-                  <div className="menu-item-text">About Me</div>
+                  <div className="menu-item-text">
+                    {polishVer ? homeBtnPl : homeBtnEn}
+                  </div>
                 </Link>
               </li>
               <li className="menu-item">
                 <Link to="#projects" className="nav-links">
                   <MdAssignment />
-                  <div className="menu-item-text">Projects</div>
+                  <div className="menu-item-text">{polishVer ? projectsBtnPl : projectsBtnEn}</div>
                 </Link>
               </li>
               <li className="menu-item">
                 <Link to="#contact" className="nav-links">
                   <MdPerson />
-                  <div className="menu-item-text">Contact</div>
+                  <div className="menu-item-text">{polishVer ? contactBtnPl : contactBtnEn}</div>
                 </Link>
               </li>
             </ul>
+            <div className="website-language">
+              <button
+                className={click ? "active-button" : "not-active-button"}
+                onClick={handleClickPL}
+              >
+                PL
+              </button>
+              <button
+                className={click ? "not-active-button" : "active-button"}
+                onClick={handleClickEN}
+              >
+                EN
+              </button>
+            </div>
           </div>
         </div>
       </>
